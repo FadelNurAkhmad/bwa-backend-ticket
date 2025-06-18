c0e91c12a38cbcc278d919a99357c65ae00451f0
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2025-06-18 08:35:04 +0700

    Update Delete API Genre

--- src/controllers/genreController.ts
+++ src/controllers/genreController.ts
@@ -21,6 +21,33 @@ export const getGenres = async (req: Request, res: Response) => {
   }
 };
 
+export const getGenreDetail = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params; // <- menggunakan destructuring assignment
+    // sama seperti const id = req.params.id;
+    // req.params adalah sebuah object
+    // req.params = {
+    //   id: "123",
+    //   name: "fadel",
+    // };
+
+    const genre = await Genre.findById(id);
+
+    return res.json({
+      data: genre,
+      message: "Success get data",
+      status: "Success",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Failed to get data",
+      data: null,
+      status: "failed",
+    });
+  }
+};
+
 export const postGenre = async (req: Request, res: Response) => {
   try {
     // Validasi data dari API
@@ -43,6 +70,56 @@ export const postGenre = async (req: Request, res: Response) => {
     console.log(error);
     return res.status(500).json({
       message: "Failed to create data",
+      data: null,
+      status: "failed",
+    });
+  }
+};
+
+export const putGenre = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const body = genreSchema.parse(req.body);
+
+    await Genre.findByIdAndUpdate(id, {
+      name: body.name,
+    });
+
+    const updatedData = await Genre.findById(id);
+
+    return res.json({
+      message: "Success update data",
+      data: updatedData,
+      status: "success",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Failed to update data",
+      data: null,
+      status: "failed",
+    });
+  }
+};
+
+export const deleteGenre = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const deletedData = await Genre.findById(id);
+
+    await Genre.findByIdAndDelete(id);
+
+    return res.json({
+      message: "Success delete data",
+      data: deletedData,
+      status: "success",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Failed to delete data",
       data: null,
       status: "failed",
     });
--- src/routes/adminRoutes/genreRoutes.ts
+++ src/routes/adminRoutes/genreRoutes.ts
@@ -1,10 +1,19 @@
 import express from "express";
-import { getGenres, postGenre } from "../../controllers/genreController";
+import {
+  deleteGenre,
+  getGenreDetail,
+  getGenres,
+  postGenre,
+  putGenre,
+} from "../../controllers/genreController";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { genreSchema } from "../../utils/zodSchema";
 const genreRoutes = express.Router();
 
 genreRoutes.get("/genres", getGenres);
+genreRoutes.get("/genres/:id", getGenreDetail);
 genreRoutes.post("/genres", validateRequest(genreSchema), postGenre);
+genreRoutes.put("/genres/:id", validateRequest(genreSchema), putGenre);
+genreRoutes.delete("/genres/:id", deleteGenre);
 
 export default genreRoutes;
